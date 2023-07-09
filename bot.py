57c90c1bf834f39540b186cafc5e90690077cb56
chocomint <keisuke20151114@gmail.com>
2023-07-10 08:03:46 +0900

    動作するところまで

--- bot.py
+++ bot.py
@@ -1,23 +1,42 @@
 import discord
+from discord.ext import commands
+import asyncio
 import os
+from voicevox import Himari
 
+FFmpeg_PATH = os.getenv('FFmpeg_PATH')
 TOKEN_DATA = os.getenv('TOKEN_DATA')
 TOKEN = TOKEN_DATA
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+# Botが準備完了したときに実行するイベント
+@bot.event
 async def on_ready():
     print('ログイン')
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
+# !speakコマンドが入力されたときに実行する関数
+@bot.command()
+async def speak(ctx, *, text):
+    if not ctx.message.author.voice:
+        await ctx.send("音声チャンネルに接続していません")
         return
-    if message.content == '/himari':
-        await message.channel.send('himaridayo')
+    # Botが既にボイスチャンネルに接続している場合
+    if ctx.voice_client is not None:
+        await ctx.voice_client.disconnect() # ボイスチャンネルから切断する
 
-client.run(TOKEN)
+    voice_channel = ctx.message.author.voice.channel
+    # ボイスチャンネルに接続する
+    vc = await voice_channel.connect()
+    # VoiceVoxでテキストを音声に変換する
+    Himari.generate_wav(text)
+    # ボイスチャンネルで音声を再生する
+    vc.play(discord.FFmpegPCMAudio(executable=FFmpeg_PATH, source='audio.wav'))
+    # 音声が再生されている間、待機する
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    await vc.disconnect()
+
+bot.run(TOKEN)
