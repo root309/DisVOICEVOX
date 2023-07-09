c5a9f4a71fa3c98cda8a5b785ed7d67d3344dfa6
chocomint <keisuke20151114@gmail.com>
2023-07-09 13:35:55 +0900

    インテンツ追加

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import discord
 import os
 
-TOKEN_DATA = os.getenv("TOKEN_DATA")
+TOKEN_DATA = os.getenv('TOKEN_DATA')
 TOKEN = TOKEN_DATA
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
 
-@client.eventasync
-def on_ready():
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
     print('ログイン')
 
 @client.event
@@ -16,6 +19,5 @@ async def on_message(message):
         return
     if message.content == '/himari':
         await message.channel.send('himaridayo')
-
 
 client.run(TOKEN)
